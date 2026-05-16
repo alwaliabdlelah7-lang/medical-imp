d9cd3e98e56aceac2b2d99852e1904b719599c71
alwaliabdlelah7-lang <alwaliabdlelah7@gmail.com>
2026-05-16 03:52:56 +0300

    Replace frontend with correct Flutter project

--- frontend_flutter/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) => MaterialApp(title: 'IMP Medical', home: Scaffold(body: Center(child: Text('النظام الطبي المتكامل'))));
-}
